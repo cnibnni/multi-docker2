e6dbf6f7e45681e60fa4b47f8ad4e0a511106288
Ogulcan Özünal <ogulcan.oezuenal@ausy-technologies.de>
2021-06-14 12:31:13 +0200

    changed app

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            change me to something meaningful pls
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
